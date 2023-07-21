fcc5e5d765e62d8eebfc71019b1ffac8ad7f952f
syeyoung <cyoung06@naver.com>
2023-07-21 14:43:29 +0900

    - list of handlers

--- src/main/kotlin/io/teammistake/chatjamo/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/io/teammistake/chatjamo/configuration/SecurityConfiguration.kt
@@ -1,6 +1,5 @@
 package io.teammistake.chatjamo.configuration
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
@@ -10,7 +9,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
+import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebExceptionHandler
+import org.springframework.web.server.handler.ExceptionHandlingWebHandler
+import reactor.core.publisher.Mono
+import java.util.function.Consumer
+import java.util.function.Function
 
 
 @EnableWebFluxSecurity
@@ -19,12 +23,31 @@ import org.springframework.web.server.WebExceptionHandler
 class SecurityConfiguration {
 
     @Bean
-    fun configure(http: ServerHttpSecurity, jwtAuthenticationWebFilter: AuthenticationWebFilter, handler: WebExceptionHandler): SecurityWebFilterChain? {
+    fun configure(http: ServerHttpSecurity, jwtAuthenticationWebFilter: AuthenticationWebFilter, handlers: List<WebExceptionHandler>): SecurityWebFilterChain? {
+        val generalHandler = {
+                exchange: ServerWebExchange, err: Throwable ->
+            var completion = Mono.error<Void>(err);
+
+            for (handler in handlers) {
+                completion = completion
+                    .doOnError { error: Throwable? ->
+                        exchange.getAttributes().put(ExceptionHandlingWebHandler.HANDLED_WEB_EXCEPTION, error)
+                    }
+                    .onErrorResume { ex: Throwable? ->
+                        handler.handle(
+                            exchange,
+                            ex!!
+                        )
+                    }
+            }
+
+            completion
+        }
         return http
             .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             .exceptionHandling {
-                it.accessDeniedHandler(handler::handle)
-                    .authenticationEntryPoint(handler::handle)
+                it.accessDeniedHandler(generalHandler)
+                    .authenticationEntryPoint(generalHandler)
             }
             .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
             .csrf {
